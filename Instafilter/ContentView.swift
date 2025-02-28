86139a0c7d21254aa397a61c93d8a19ffb622264
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-28 15:01:57 +0530

    Added syntax to add some effects on imagea and alos ContentUnavailable view usages

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,16 +6,84 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            image?
+//                .resizable()
+//                .scaledToFit()
+            ContentUnavailableView("No snippets", systemImage: "swift")
+            ContentUnavailableView("No snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet."))
+            ContentUnavailableView {
+                Label("No snippets", systemImage: "swift")
+            } description: {
+                Text("You don't have any saved snippets yet.")
+            } actions: {
+                Button("Create Snippet") {
+                    // create a snippet
+                }
+                .buttonStyle(.borderedProminent)
+            }
+
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        let inputImage = UIImage(resource: .room)
+           let beginImage = CIImage(image: inputImage)
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+//        
+//        let currentFilter = CIFilter.pixellate()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.scale = 100
+        
+        
+//        let currentFilter = CIFilter.crystallize()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.radius = 200
+        
+        
+//        let currentFilter = CIFilter.twirlDistortion()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.radius = 1000
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        
+        
+        // get a CIImage from our filter or exit if that fails beacuse they return optional
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        // attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        // convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        image = Image(uiImage: uiImage)
+
+        
+        
     }
 }
 
