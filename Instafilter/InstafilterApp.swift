09b6faba6f8f0fad61faa43770f60eddc5c5a429
shashankb-cc <shashank.b@codecraft.co.in>
2025-02-28 12:43:30 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Shashank B on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
